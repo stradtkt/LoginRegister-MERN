77e039313ffea7cd828555e66d53262b3a1b0f43
Kevin <stradtkt22@gmail.com>
2019-06-17 14:51:29 -0400

    Added the database

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
