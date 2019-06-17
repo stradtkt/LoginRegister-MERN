3e82f272f3109450cda652364575bcdeb88902b3
Kevin <stradtkt22@gmail.com>
2019-06-17 15:24:44 -0400

    Added the login and register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import Login from './components/Login';
+import Register from './components/Register';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Login}/>
+                <Route exact path="/register" component={Register}/>
+            </Switch>
+        </Router>
+    </Provider>
   );
 }
 
